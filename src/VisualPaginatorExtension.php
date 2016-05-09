b4b454325949d4e5e6adefea93277d36cd631fe7
Aleš <aleswita+githubcom@gmail.com>
2016-05-09 08:19:25 +0200

    Create VisualPaginatorExtension.php

---
+++ src/VisualPaginatorExtension.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * This file is part of the AlesWita\VisualPaginator
+ * Copyright (c) 2015 Ales Wita (aleswita@gmail.com)
+ */
+
+namespace AlesWita\Components;
+
+use Nette;
+
+
+/**
+ * @author Aleš Wita
+ */
+class VisualPaginatorExtension extends Nette\DI\CompilerExtension
+{
+	/** @var array */
+	public $defaults = [
+		"session" => NULL,
+		"translator" => NULL,
+		"template" => VisualPaginator::TEMPLATE_NORMAL,
+		"texts" => NULL,
+	];
+
+	public function loadConfiguration()
+	{
+		$config = $this->getConfig($this->defaults);
+		$container = $this->getContainerBuilder();
+
+		$vp = $container->addDefinition($this->prefix("visualpaginator"))
+			->setClass("AlesWita\Components\VisualPaginator");
+
+		if ($config["session"] !== NULL) {
+			$vp->addSetup('$service->setSession(?)', [$config["session"]]);
+		}
+		if ($config["translator"] !== NULL) {
+			$vp->addSetup('$service->setTranslator(?)', [$config["translator"]]);
+		}
+		if ($config["template"] !== VisualPaginator::TEMPLATE_NORMAL) {
+			$vp->addSetup('$service->setPaginatorTemplate(?)', [$config["template"]]);
+		}
+		if ($config["texts"] !== NULL) {
+			foreach ($config["texts"] as $arr) {
+				$vp->addSetup('$service->setText(?,?)', [$arr[0], $arr[1]]);
+			}
+		}
+	}
+}
