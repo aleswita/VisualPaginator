739576f8ce3fe736a70cd67f7ea6bb55a6978748
Aleš <aleswita+githubcom@gmail.com>
2017-05-31 12:14:46 +0200

    Create bootstrap.php

---
+++ tests/bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of the AlesWita\Components\VisualPaginator
+ * Copyright (c) 2015 Ales Wita (aleswita+github@gmail.com)
+ */
+
+declare(strict_types=1);
+
+require __DIR__ . "/../../../autoload.php";
+
+if (!class_exists("Tester\\Assert")) {
+	echo "Install Nette Tester using `composer update --dev`";
+	exit(1);
+}
+
+
+Tester\Environment::setup();
+
+define("TEMP_DIR", __DIR__ . "/temp/" . (isset($_SERVER["argv"]) ? md5(serialize($_SERVER["argv"])) : getmypid()));
+@mkdir(dirname(TEMP_DIR));
+@mkdir(TEMP_DIR);
