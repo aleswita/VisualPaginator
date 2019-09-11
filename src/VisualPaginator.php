5303ff0641fe40bdb31ad01322d8bec51d09c9cc
Jan Tojnar <jtojnar@gmail.com>
2019-09-11 04:03:55 +0200

    Fix rendering
    I was getting the following error:
    
    	Argument 1 passed to Nette\Utils\Paginator::setPage() must be of the type int, null given (via SmartObject)

--- src/VisualPaginator.php
+++ src/VisualPaginator.php
@@ -232,7 +232,7 @@ class VisualPaginator extends Application\UI\Control
 			}
 		}
 
-		$this->getPaginator()->page = $this->page;
+		$this->getPaginator()->page = ($this->page === NULL ? 1 : $this->page);
 		$this->getPaginator()->itemsPerPage = $this->itemsPerPage;
 		$this->page = $this->getPaginator()->page;
 		$this->itemsPerPage = $this->getPaginator()->itemsPerPage;
